ca3fc0e855e219458706142a8f5d10eed7af6479
Alex Colombo <uricaine@hotmail.com>
2019-01-23 14:59:50 +0100

    convenciones

--- variables.py
+++ variables.py
@@ -1,3 +1,21 @@
 ### VARIABLES ###
 
+# case sensitive
+# no se puede empezar con numero
+
 nombre = "Alex"
+x = 100
+book = "El señor de los anillos"
+
+#Se puede definir las variables en una sola linea
+z, libro = 50, "Juego de tronos" 
+print(libro)
+
+#Convencones
+nombre_libro = "Yo robot" # Sanke case
+nombreLibro = "Psicosis" # Camel case
+NombreLibro = "Goethe" # Pascal case
+
+# Constantes por convencion iria todo en mayusculas
+MINOMBRE = "Alex"
+PI = 3.14 
