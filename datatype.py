26ab434214edb2f22e78d2b2b126d189eb73116e
Alex Colombo <uricaine@hotmail.com>
2019-01-23 14:49:27 +0100

    tipo de datos

---
+++ datatype.py
@@ -0,0 +1,38 @@
+#Strings
+print('Hola ' + 'Alex Colombo')
+print("Hola Alex")
+print("""Hola Alex""")
+print('''Hola 
+Alex''')
+#Type
+print(type('Hola Alex'))
+#Integer
+print(30)   
+#Float
+print(30.5)  
+#Boolean
+print(type(True))
+print(type(False))
+#List
+print(type([10,20,False, "hola"]))
+#Tuples (inmutable)
+print(type((10,20,False, "hola")))
+#Dictionaries (key value pair)
+print(type({
+    "nombre":"Alex",
+    "apellido": "Colombo",
+    "avatar": "SkyWalker"
+    }))
+#None
+print(type(None))
+
+
+
+
+
+
+
+
+
+
+
