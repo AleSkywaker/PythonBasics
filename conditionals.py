db9a832e0c459c4ce0293c767a602f98859edca5
Alex Colombo <uricaine@hotmail.com>
2019-01-25 19:16:34 +0100

    init  conditionals

--- conditionals.py
+++ conditionals.py
@@ -1,10 +1,10 @@
-x = 50
+x = 5
 
 if x > 30:
     print(f"{x} es mayor que 30")
 elif x == 20:
     print(f"{x} es 20")
 else:
-    print(f"{x} es menos que 30")
+    print(f"{x} es menor que 30")
 
     
