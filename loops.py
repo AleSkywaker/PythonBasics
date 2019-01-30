e7b72826754f83a23128ab99d1cedc15c47de6e8
Alex Colombo <uricaine@hotmail.com>
2019-01-30 10:43:59 +0100

    loops

--- loops.py
+++ loops.py
@@ -3,5 +3,6 @@ alimentos = ["banana", "fresas", "yogurt", "arroz"]
 for alimento in alimentos:
     if alimento == "banana":
         print("tienes que comprar " + alimento)
+    print(alimento)
 
 
